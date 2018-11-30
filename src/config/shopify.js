162d24e54c5b59246ab5cffaf3d2bed0c67598f6
Adam Bohannon <abo46n2@gmail.com>
2018-11-30 12:03:21 -0800

    Added shopify api call limit

--- src/config/shopify.js
+++ src/config/shopify.js
@@ -6,14 +6,20 @@ const prod = ENV === 'production'
 
 let params
 
+// Limit the number of API calls so we don't get a 429 bucket overflor error
+// more info: https://help.shopify.com/en/api/getting-started/api-call-limit
+const autoLimit = { calls: 2, interval: 1000, bucketSize: 30 }
+
 if (dev) {
   params = {
+    autoLimit,
     shopName: process.env.DEV_SHOPIFY_STORE_NAME,
     apiKey: process.env.DEV_SHOPIFY_API_KEY,
     password: process.env.DEV_SHOPIFY_API_PASSWORD,
   }
 } else if (prod) {
   params = {
+    autoLimit,
     shopName: process.env.PROD_SHOPIFY_STORE_NAME,
     apiKey: process.env.PROD_SHOPIFY_API_KEY,
     password: process.env.PROD_SHOPIFY_API_PASSWORD,
